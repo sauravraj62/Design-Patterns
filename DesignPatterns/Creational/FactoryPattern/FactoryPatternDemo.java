d6cd1213ac235b2193c680a4005c7cf16268d557
Saurav Raj <sauravraj62@gmail.com>
2022-11-06 19:01:53 +0530

    Added Definition
    Added Definition

--- DesignPatterns/Creational/FactoryPattern/FactoryPatternDemo.java
+++ DesignPatterns/Creational/FactoryPattern/FactoryPatternDemo.java
@@ -1,3 +1,6 @@
+//Factory Method is a creational design pattern that provides an interface for creating objects in a superclass,
+//but allows subclasses to alter the type of objects that will be created.
+
 public class FactoryPatternDemo {
 
     public static void main(String[] args) {
